32658beb660ce15e41bd9a78c270d839df3e48f5
AmitMishra-852 <amitrs852852@gmail.com>
2021-11-11 00:03:42 +0530

    refector

--- src/screen/Profilescreen.js
+++ src/screen/Profilescreen.js
@@ -5,19 +5,16 @@ import { useParams } from "react-router"
 import Righthomepage from "../component/Righthomepage";
 import Header from '../component/Header';
 import axios from "axios";
-import { StateHandler } from "../component/login/context/Authcontext";
+// import { StateHandler } from "../component/login/context/Authcontext";
 import { Link } from "react-router-dom";
 
 
 
 function Profilescreen({ click }) {
     const { userName } = useParams();
-    const { user: currentUser, dispatch } = StateHandler()
+    // const { user: currentUser, dispatch } = StateHandler()
     const [profileUser, setProfileUser] = useState({});
-    // console.log("user", currentUser.following)
-    // console.log("profile-user", profileUser?._id)
     const [profilePost, setProfilePost] = useState([]);
-    const [ followed , setFollowed] = useState(null)
     console.log(followed)
   
 
@@ -38,43 +35,6 @@ function Profilescreen({ click }) {
         }
         getAllPost()
     }, [profileUser?.userName])
-
-
-
-
-    const ProfileFollowHandler =async(e)=>{
-
-        e.preventDefault()
-        console.log(currentUser.following)
-        console.log(currentUser.following.includes(profileUser?._id))
-        console.log("profile-user", profileUser?._id)
-
-
-        // if(currentUser.following.includes(profileUser?._id)){
-        //     const res = await axios.put(`https://mediaAppBackend.jerryroy.repl.co/api/user/${follower._id}/unfollow`,{
-        //         userId:user._id   
-        //     })
-        //     setFollower(res.data)
-        //     setTimeout(()=>{
-        //         setFollower("")
-        //     },3000)
-        //     dispatch({ type: "UNFOLLOW", payLoad: follower._id })
-
-        // }else{
-        //     const res = await axios.put(`https://mediaAppBackend.jerryroy.repl.co/api/user/${follower._id}/follow`,{
-        //         userId:user._id
-        //     })
-        //     setFollower(res.data)
-        //     setTimeout(()=>{
-        //         setFollower("")
-        //     },3000)
-        //     dispatch({ type: "FOLLOW", payLoad: follower._id })
-
-        // }
-        setFollowed(!currentUser.following.includes(profileUser?._id))
-    }
-    console.log(followed)
-
     
       
     return (
@@ -99,7 +59,6 @@ function Profilescreen({ click }) {
                                 <span className="userInfo"><span className="info-bold">{profileUser.followers?.length}</span> followers</span>
                                 <span className="userInfo"><span className="info-bold">{profileUser.following?.length}</span> following</span>
                             </div>
-                            <button className="followbtn" onClick={ProfileFollowHandler}> Follow </button>
                          
                         </div>
                     </div>
